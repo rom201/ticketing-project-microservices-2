722fec9afa292db53db9ec48fed7d5b42579aafa
rom201 <kovalchukr1104@gmail.com>
2023-08-27 22:17:04 -0400

    added modules

---
+++ app-client-management/src/main/java/com/cydeo/AppClientManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClientManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClientManagementApplication.class, args);
+    }
+
+}
---
+++ app-domain-model/src/main/java/com/cydeo/AppDomainModelApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDomainModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDomainModelApplication.class, args);
+    }
+
+}
