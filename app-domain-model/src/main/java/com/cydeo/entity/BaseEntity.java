8ebdc7010fce9426e61706f4d81ce9e639578e5d
rom201 <kovalchukr1104@gmail.com>
2023-08-27 22:35:14 -0400

    added modules and updated parent

---
+++ app-domain-model/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true,updatable = false)
+    public LocalDateTime insertDateTime;
+    @Column(nullable = true,updatable = false)
+    public Long insertUserId;
+    @Column(nullable = true)
+    public LocalDateTime lastUpdateDateTime;
+    @Column(nullable = true)
+    public Long lastUpdateUserId;
+
+    private Boolean isDeleted=false;
+
+}
---
+++ user-service/src/main/java/com/cedeo/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.cedeo.repository;
+
+import com.cydeo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Role findByDescription(String description);
+}
